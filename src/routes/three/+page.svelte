4997a9f2d5d7d478d90b0956b54b1b8cc640005e
chayapatr <chayapatr.arc@gmail.com>
2024-06-19 21:00:00 +0700

    feat: minor edit

--- src/routes/three/+page.svelte
+++ src/routes/three/+page.svelte
@@ -68,7 +68,7 @@
 
 		loadModel = (i) => {
 			loader.load(
-				`/glb/subin${i}.glb`,
+				`/sample.glb`,
 				function (gltf) {
 					for (let i = 0; i < scene.children.length; i++) {
 						if (scene.children[i].type === 'Group') scene.remove(scene.children[i]);
